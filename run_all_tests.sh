c2f591d2dce755e5cbfa1e14bf045e98ceb83ea3
james spargo <spargonaut@gmail.com>
2016-02-19 16:39:07 -0600

    JS|DN - add commands to exit the run_all_tests script when a gradle task fails.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -3,19 +3,19 @@
 #set -e
 
 echo '**********        deploying the UI          **********'
-./gradlew deployUI --quiet
+./gradlew deployUI --quiet  || exit $?
 
 echo '**********      cleaning the database       **********'
-./gradlew flywayClean --quiet
+./gradlew flywayClean --quiet  || exit $?
 
 echo '**********   running database migrations    **********'
-./gradlew flywayMigrate --quiet
+./gradlew flywayMigrate --quiet  || exit $?
 
 echo '**********     building the application     **********'
-./gradlew clean build shadowJar --quiet
+./gradlew clean build shadowJar --quiet  || exit $?
 
 echo '**********  Running the Integration Tests   **********'
-./gradlew integrationTest --quiet
+./gradlew integrationTest --quiet  || exit $?
 
 echo '**********   running the functional tests   **********'
-./gradlew functionalTest --quiet
+./gradlew functionalTest --quiet  || exit $?
