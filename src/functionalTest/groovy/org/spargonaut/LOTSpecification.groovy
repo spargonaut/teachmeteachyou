ab2f3136713c118cd019b5f2af87def2496abaef
jamesspargo <jspargo@thoughtworks.com>
2016-04-16 00:47:35 -0500

    JS - refactor out the functionality of the interested user from the functional test; refactor out the functionality of the teacher user from the functional test;

--- src/functionalTest/groovy/org/spargonaut/LOTSpecification.groovy
+++ src/functionalTest/groovy/org/spargonaut/LOTSpecification.groovy
@@ -2,7 +2,6 @@ package org.spargonaut
 
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
-import org.openqa.selenium.WebElement
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
@@ -17,6 +16,12 @@ class LOTSpecification extends Specification {
     private static final String NAME_DISPLAY_CLASSNAME = 'name_display'
     private static final String WORKSHOP_DETAILS_HEADER_ID = 'workshop_header'
 
+    private static final String WORKSHOP_CREATOR = 'aloicious abercrombie'
+    private static final String WORKSHOP_NAME  = 'some new workshop'
+    private static final String WORKSHOP_DETAILS = 'these are some more details'
+    private static final String WORKSHOP_FORM_BUTTON_ID = 'workshop_form_button'
+    private static final int MAX_TIME_TO_WAIT_FOR_ELEMENT = 2
+
     WebDriver driver = new FirefoxDriver()
 
     void setup() {
@@ -27,30 +32,24 @@ class LOTSpecification extends Specification {
         driver.quit()
     }
 
-    void 'a user can create a new workshop request and have that information show up below the submit button'() {
-        given: 'a user who wants to create a new workshop'
-        String userOne = 'aloicious abercrombie'
-        String userTwo = 'mike smith'
-        String userThree = 'bob roger'
-        String workshopName = 'some new workshop'
-        String workshopDtls = 'these are some more details'
+    void 'a user can create a new workshop request and see the details of that workshop' () {
+        given: 'a user clicks the create workshop button'
+        driver.findElement(By.id(WORKSHOP_FORM_BUTTON_ID)).click();
 
-        when: 'the user clicks the create workshop button'
-        driver.findElement(By.id('workshop_form_button')).click();
-
-        then: 'the create workshop form is visible'
+        expect: 'the create workshop form is visible'
         ExpectedConditions.presenceOfElementLocated(By.id(WORKSHOP_FORM_ID))
 
         when: 'the user fills in the form with that information and clicks the create button'
-        submitWorkshopInformation(userOne, workshopName, workshopDtls)
+        submitWorkshopInformation(WORKSHOP_CREATOR, WORKSHOP_NAME, WORKSHOP_DETAILS)
 
         then: 'the new workshop shows up in the workshop list'
-        int maxTimeToWaitForElement = 2
-        WebDriverWait wait = new WebDriverWait(driver, maxTimeToWaitForElement)
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.className(NAME_DISPLAY_CLASSNAME), userOne))
+        WebDriverWait wait = new WebDriverWait(driver, MAX_TIME_TO_WAIT_FOR_ELEMENT)
         wait.until(
                 ExpectedConditions.textToBePresentInElementLocated(
-                        By.className(WORKSHOP_TITLE_CLASSNAME), workshopName))
+                        By.className(NAME_DISPLAY_CLASSNAME), WORKSHOP_CREATOR))
+        wait.until(
+                ExpectedConditions.textToBePresentInElementLocated(
+                        By.className(WORKSHOP_TITLE_CLASSNAME), WORKSHOP_NAME))
 
         and: 'the workshop form is no longer visible'
         wait.until(ExpectedConditions.not(
@@ -60,39 +59,62 @@ class LOTSpecification extends Specification {
         driver.findElement(By.className(WORKSHOP_TITLE_CLASSNAME)).click()
 
         then: 'the details of that workshop are displayed on the page'
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id(WORKSHOP_DETAILS_HEADER_ID), userOne))
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id(WORKSHOP_DETAILS_HEADER_ID), workshopName))
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id('workshop_details_details'), workshopDtls))
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(
+                By.id(WORKSHOP_DETAILS_HEADER_ID), WORKSHOP_CREATOR))
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id(WORKSHOP_DETAILS_HEADER_ID), WORKSHOP_NAME))
+        wait.until(
+                ExpectedConditions.textToBePresentInElementLocated(By.id('workshop_details_details'), WORKSHOP_DETAILS))
 
         when: 'the user clicks the done button'
         driver.findElement(By.id('done_button')).click()
 
         then: 'the main workshop page loads again'
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.className(NAME_DISPLAY_CLASSNAME), userOne))
         wait.until(
                 ExpectedConditions.textToBePresentInElementLocated(
-                        By.className(WORKSHOP_TITLE_CLASSNAME), workshopName))
+                        By.className(NAME_DISPLAY_CLASSNAME), WORKSHOP_CREATOR))
+        wait.until(
+                ExpectedConditions.textToBePresentInElementLocated(
+                        By.className(WORKSHOP_TITLE_CLASSNAME), WORKSHOP_NAME))
+    }
 
-        when: 'a second user clicks a workshop to view the details of that workshop'
+    void 'a user can indicate they are interested in a previously created workshop' () {
+        given: 'a workshop is already created'
+        String interestedUser = 'Michael Stipe'
+        driver.findElement(By.id(WORKSHOP_FORM_BUTTON_ID)).click();
+        submitWorkshopInformation(WORKSHOP_CREATOR, WORKSHOP_NAME, WORKSHOP_DETAILS)
+
+        and: 'the user views the details of the workshop'
         driver.findElement(By.className(WORKSHOP_TITLE_CLASSNAME)).click()
 
-        then: 'the details of that workshop are displayed on the page again'
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id(WORKSHOP_DETAILS_HEADER_ID), userOne))
+        when: 'the user adds their name to the interested list'
+        driver.findElement(By.id('add_interest_input')).sendKeys(interestedUser)
 
-        when: 'the second user adds their name to the list of interested people'
-        driver.findElement(By.id('add_interest_input')).sendKeys(userTwo)
+        and: 'clicks Im interested'
         driver.findElement(By.id('add_interest_button')).click()
 
-        then: 'that users name shows up on the interested list'
-        List<WebElement> interestedPeople = driver.findElements(By.xpath('.//*'))
-        wait.until(ExpectedConditions.textToBePresentInElement(interestedPeople.first(), userTwo))
+        then: 'that users name appears in the interested users list'
+        WebDriverWait wait = new WebDriverWait(driver, MAX_TIME_TO_WAIT_FOR_ELEMENT)
+        wait.until(
+                ExpectedConditions.textToBePresentInElementLocated(
+                        By.xpath('//div[@id=\'interested_people\']/div[last()]'), interestedUser))
+    }
 
-        when: 'a third user adds their name saying they can teach that workshop'
-        driver.findElement(By.id('add_teacher_input')).sendKeys(userThree)
+    void 'a user can indicate that they can teach a workshop' () {
+        given: 'a workshop is already created'
+        String teacherUser = 'peter buck'
+        driver.findElement(By.id(WORKSHOP_FORM_BUTTON_ID)).click();
+        submitWorkshopInformation(WORKSHOP_CREATOR, WORKSHOP_NAME, WORKSHOP_DETAILS)
+
+        and: 'the user views the details of the workshop'
+        driver.findElement(By.className(WORKSHOP_TITLE_CLASSNAME)).click()
+
+        when: 'that user indicates they can teach that class'
+        driver.findElement(By.id('add_teacher_input')).sendKeys(teacherUser)
         driver.findElement(By.id('add_teacher_button')).click()
 
-        then: 'that users name show up as being able to teach that workshop'
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.className('instructor'), userThree))
+        then: 'that users name appears as signed up to teach that workshop'
+        WebDriverWait wait = new WebDriverWait(driver, MAX_TIME_TO_WAIT_FOR_ELEMENT)
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.className('instructor'), teacherUser))
     }
 
     private void submitWorkshopInformation(String userName, String workshopName, String workshopDtls) {
